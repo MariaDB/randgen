0544d4fddeaab821a611ace90069e6d3d7e63886
Elena Stepanova <elenst@montyprogram.com>
2014-09-02 02:13:05 +0400

    Distinguish stress combinations with randomized config and general tests

--- conf/mariadb/10.0-random.cc
+++ conf/mariadb/10.0-random.cc
@@ -4,19 +4,14 @@ $combinations = [
 	'
 		--no-mask
 		--seed=time
-		--threads=8
+		--threads=32
 		--duration=600
 		--queries=100M
 		--reporters=QueryTimeout,Backtrace,ErrorLog,Deadlock
-		--validators=TransformerNoComparator
-		--transformers=ExecuteAsDeleteReturning,ExecuteAsPreparedTwice,DisableOptimizations,EnableOptimizations,OrderBy
 		--redefine=conf/mariadb/general-workarounds.yy
 		--redefine=conf/mariadb/10.0-features-redefine.yy
 		--mysqld=--log_output=FILE
-		--mysqld=--slow_query_log
-		--mysqld=--long_query_time=0.000001
-		--mysqld=--log_bin_trust_function_creators=1
-		--mysqld=--query_cache_size=64M
+		--genconfig=conf/mariadb/10.0.cnf.template
 	'], 
 	[
 		'--views --grammar=conf/runtime/metadata_stability.yy --gendata=conf/runtime/metadata_stability.zz',
@@ -40,26 +35,5 @@ $combinations = [
 		'--engine=TokuDB --mysqld=--plugin-load=ha_tokudb.so --mysqld=--loose-tokudb',
 		'--engine=InnoDB --mysqld=--ignore-builtin-innodb --mysqld=--plugin-load=ha_innodb.so'
 	],
-# slave-skip-errors: 
-# 1054: MySQL:67878 (LOAD DATA in views)
-# 1317: Query partially completed on the master (MDEV-368 which won't be fixed)
-# 1049, 1305, 1539: MySQL:65428 (Unknown database) - fixed in 5.7.0
-# 1505: MySQL:64041 (Partition management on a not partitioned table) 
-	[
-		'--rpl_mode=row --mysqld=--slave-skip-errors=1049,1305,1539,1505',
-		'--rpl_mode=statement --mysqld=--slave-skip-errors=1054,1317,1049,1305,1539,1505',
-		'--rpl_mode=mixed --mysqld=--slave-skip-errors=1049,1305,1539,1505,1317',
-	],
-	['
-		--use-gtid=current_pos
-		--mysqld=--optimizer_switch=extended_keys=on,exists_to_in=on 
-		--mysqld=--use_stat_tables=PREFERABLY
-		--mysqld=--optimizer_selectivity_sampling_limit=100 
-		--mysqld=--optimizer_use_condition_selectivity=5 
-		--mysqld=--histogram_size=100 
-		--mysqld=--histogram_type=DOUBLE_PREC_HB
-		--mysqld=--log_slow_verbosity=query_plan,explain
-		--mysqld=--slave_parallel_threads=8
-	'],
 ];
 
