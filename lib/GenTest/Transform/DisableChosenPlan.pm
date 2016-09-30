1ead3b4c8a38120f254bd710132ccd4030ba4c5e
Elena Stepanova <elenst@montyprogram.com>
2016-10-01 02:26:59 +0300

    Clean up identation and sorting

--- lib/GenTest/Transform/DisableChosenPlan.pm
+++ lib/GenTest/Transform/DisableChosenPlan.pm
@@ -1,4 +1,5 @@
 # Copyright (c) 2008, 2012 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2016, MariaDB Corporation Ab
 # Use is subject to license terms.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -40,29 +41,29 @@ use Data::Dumper;
 #
 
 my @explain2switch = (
-	[ 'sort_intersect'	=> "optimizer_switch='index_merge_sort_intersection=off'"],
-	[ 'intersect'		=> "optimizer_switch='index_merge_intersection=off'"],
-	[ 'firstmatch'		=> "optimizer_switch='firstmatch=off'" ],
-	[ '<expr_cache>'	=> "optimizer_switch='subquery_cache=off'" ],
-	[ 'materializ'		=> "optimizer_switch='materialization=off,in_to_exists=on'" ],
-	[ 'semijoin'		=> "optimizer_switch='semijoin=off'" ],
-	[ 'Start temporary'     => "optimizer_switch='semijoin=off'" ],
-	[ 'loosescan'		=> "optimizer_switch='loosescan=off'" ],
-	[ '<subquery'		=> "optimizer_switch='materialization=off,in_to_exists=on'" ],
-	[ '<exists>'		=> "optimizer_switch='in_to_exists=off,materialization=on'" ],
-	[ qr{hash|BNLH|BKAH}	=> "optimizer_switch='join_cache_hashed=off'" ],	
-	[ 'BKA'			=> "optimizer_switch='join_cache_bka=off'" ],
-	[ 'incremental'		=> "optimizer_switch='join_cache_incremental=off'" ],
-	[ 'join buffer'		=> "join_cache_level=0" ],
-	[ 'join buffer'		=> "optimizer_join_cache_level=0" ],
-	[ 'mrr'			=> "optimizer_switch='mrr=off'" ],
-	[ 'index condition'	=> "optimizer_switch='index_condition_pushdown=off'" ],
-	[ qr{DERIVED}s		=> "optimizer_switch='derived_merge=on'" ],
-	[ qr{(?!DERIVED)}s	=> "optimizer_switch='derived_merge=off'" ],
-	[ qr{key[0-9]}		=> "optimizer_switch='derived_with_keys=off'" ],
-	[ 'Key-ordered'		=> "optimizer_switch='mrr_sort_keys=off'" ],
-	[ 'Key-ordered'		=> "optimizer_switch='mrr=off'" ],
-	[ 'Rowid-ordered'	=> "optimizer_switch='mrr=off'" ]
+    [ 'sort_intersect'    => "optimizer_switch='index_merge_sort_intersection=off'"],
+    [ 'intersect'        => "optimizer_switch='index_merge_intersection=off'"],
+    [ 'firstmatch'        => "optimizer_switch='firstmatch=off'" ],
+    [ '<expr_cache>'    => "optimizer_switch='subquery_cache=off'" ],
+    [ 'materializ'        => "optimizer_switch='materialization=off,in_to_exists=on'" ],
+    [ 'semijoin'        => "optimizer_switch='semijoin=off'" ],
+    [ 'Start temporary'     => "optimizer_switch='semijoin=off'" ],
+    [ 'loosescan'        => "optimizer_switch='loosescan=off'" ],
+    [ '<subquery'        => "optimizer_switch='materialization=off,in_to_exists=on'" ],
+    [ '<exists>'        => "optimizer_switch='in_to_exists=off,materialization=on'" ],
+    [ qr{hash|BNLH|BKAH}    => "optimizer_switch='join_cache_hashed=off'" ],    
+    [ 'BKA'            => "optimizer_switch='join_cache_bka=off'" ],
+    [ 'incremental'        => "optimizer_switch='join_cache_incremental=off'" ],
+    [ 'join buffer'        => "join_cache_level=0" ],
+    [ 'join buffer'        => "optimizer_join_cache_level=0" ],
+    [ 'mrr'            => "optimizer_switch='mrr=off'" ],
+    [ 'index condition'    => "optimizer_switch='index_condition_pushdown=off'" ],
+    [ qr{DERIVED}s        => "optimizer_switch='derived_merge=on'" ],
+    [ qr{(?!DERIVED)}s    => "optimizer_switch='derived_merge=off'" ],
+    [ qr{key[0-9]}        => "optimizer_switch='derived_with_keys=off'" ],
+    [ 'Key-ordered'        => "optimizer_switch='mrr_sort_keys=off'" ],
+    [ 'Key-ordered'        => "optimizer_switch='mrr=off'" ],
+    [ 'Rowid-ordered'    => "optimizer_switch='mrr=off'" ]
 );
 
 my %explain2count;
@@ -70,62 +71,62 @@ my %explain2count;
 my $available_switches;
 
 sub transform {
-	my ($class, $original_query, $executor) = @_;
+    my ($class, $original_query, $executor) = @_;
 
-	if (not defined $available_switches) {
-		my $dbh_probe = DBI->connect($executor->dsn(), undef, undef, { PrintError => 0 } );
-		
-		foreach my $explain2switch (@explain2switch) {
-			my ($explain_fragment, $optimizer_switch) = ($explain2switch->[0], $explain2switch->[1]);
-			my $sth_probe = $dbh_probe->prepare("SET SESSION $optimizer_switch");
-			$sth_probe->execute();
-			$available_switches->{$optimizer_switch}++ if not defined $sth_probe->err();
-		}
-	}
+    if (not defined $available_switches) {
+        my $dbh_probe = DBI->connect($executor->dsn(), undef, undef, { PrintError => 0 } );
+        
+        foreach my $explain2switch (@explain2switch) {
+            my ($explain_fragment, $optimizer_switch) = ($explain2switch->[0], $explain2switch->[1]);
+            my $sth_probe = $dbh_probe->prepare("SET SESSION $optimizer_switch");
+            $sth_probe->execute();
+            $available_switches->{$optimizer_switch}++ if not defined $sth_probe->err();
+        }
+    }
 
-	# We skip: - [OUTFILE | INFILE] queries because these are not data producing and fail (STATUS_ENVIRONMENT_FAILURE)
-		return STATUS_WONT_HANDLE if $original_query =~ m{(OUTFILE|INFILE|PROCESSLIST|INTO)}sio
-		|| $original_query !~ m{^\s*SELECT}sio;
+    # We skip: - [OUTFILE | INFILE] queries because these are not data producing and fail (STATUS_ENVIRONMENT_FAILURE)
+        return STATUS_WONT_HANDLE if $original_query =~ m{(OUTFILE|INFILE|PROCESSLIST|INTO)}sio
+        || $original_query !~ m{^\s*SELECT}sio;
 
-	my $original_explain = $executor->execute("EXPLAIN EXTENDED $original_query");
+    my $original_explain = $executor->execute("EXPLAIN EXTENDED $original_query");
 
-	if ($original_explain->status() == STATUS_SERVER_CRASHED) {
-		return STATUS_SERVER_CRASHED;
-	} elsif ($original_explain->status() ne STATUS_OK) {
-		say("Query: $original_query EXPLAIN failed: ".$original_explain->err()." ".$original_explain->errstr());
-		return $original_explain->status();
-	}
+    if ($original_explain->status() == STATUS_SERVER_CRASHED) {
+        return STATUS_SERVER_CRASHED;
+    } elsif ($original_explain->status() ne STATUS_OK) {
+        say("Query: $original_query EXPLAIN failed: ".$original_explain->err()." ".$original_explain->errstr());
+        return $original_explain->status();
+    }
 
-	my $original_explain_string = Dumper($original_explain->data())."\n".Dumper($original_explain->warnings());
+    my $original_explain_string = Dumper($original_explain->data())."\n".Dumper($original_explain->warnings());
 
-	my @transformed_queries;
-	foreach my $explain2switch (@explain2switch) {
-		my ($explain_fragment, $optimizer_switch) = ($explain2switch->[0], $explain2switch->[1]);
-		next if not exists $available_switches->{$optimizer_switch};
-		if ($original_explain_string =~ m{$explain_fragment}si) {
-			$explain2count{"$explain_fragment => $optimizer_switch"}++;
-			my ($switch_name) = $optimizer_switch =~ m{^(.*?)=}sgio;
-			push @transformed_queries, [
-				'SET @switch_saved = @@'.$switch_name.';',
-				"SET SESSION $optimizer_switch;",
-				"$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */ ;"
-			];
-			push @transformed_queries, [ '/* TRANSFORM_CLEANUP */ SET SESSION '.$switch_name.'=@switch_saved' ];
-		}
-	}
+    my @transformed_queries;
+    foreach my $explain2switch (@explain2switch) {
+        my ($explain_fragment, $optimizer_switch) = ($explain2switch->[0], $explain2switch->[1]);
+        next if not exists $available_switches->{$optimizer_switch};
+        if ($original_explain_string =~ m{$explain_fragment}si) {
+            $explain2count{"$explain_fragment => $optimizer_switch"}++;
+            my ($switch_name) = $optimizer_switch =~ m{^(.*?)=}sgio;
+            push @transformed_queries, [
+                'SET @switch_saved = @@'.$switch_name.';',
+                "SET SESSION $optimizer_switch;",
+                "$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */ ;"
+            ];
+            push @transformed_queries, [ '/* TRANSFORM_CLEANUP */ SET SESSION '.$switch_name.'=@switch_saved' ];
+        }
+    }
 
-	if ($#transformed_queries > -1) {
-		return \@transformed_queries;
-	} else {
-		return STATUS_WONT_HANDLE;
-	}
+    if ($#transformed_queries > -1) {
+        return \@transformed_queries;
+    } else {
+        return STATUS_WONT_HANDLE;
+    }
 }
 
 sub DESTROY {
-	if (rqg_debug()) {
-		say("DisableChosenPlan statistics:");
-		print Dumper \%explain2count;
-	}
+    if (rqg_debug()) {
+        say("DisableChosenPlan statistics:");
+        print Dumper \%explain2count;
+    }
 }
 
 1;
--- lib/GenTest/Validator/QueryProperties.pm
+++ lib/GenTest/Validator/QueryProperties.pm
@@ -30,53 +30,53 @@ use GenTest::Result;
 use GenTest::Validator;
 
 my @properties = (
-	'RESULTSET_HAS_SAME_DATA_IN_EVERY_ROW',
-	'RESULTSET_IS_SINGLE_INTEGER_ONE',
-	'RESULTSET_HAS_ZERO_OR_ONE_ROWS',
-	'RESULTSET_HAS_ONE_ROW',
-	'QUERY_IS_REPLICATION_SAFE'
+    'RESULTSET_HAS_SAME_DATA_IN_EVERY_ROW',
+    'RESULTSET_IS_SINGLE_INTEGER_ONE',
+    'RESULTSET_HAS_ZERO_OR_ONE_ROWS',
+    'RESULTSET_HAS_ONE_ROW',
+    'QUERY_IS_REPLICATION_SAFE'
 );
 
 my %properties;
 foreach my $property (@properties) {
-	$properties{$property} = 1;
+    $properties{$property} = 1;
 };
 
 sub validate {
-	my ($validator, $executors, $results) = @_;
+    my ($validator, $executors, $results) = @_;
 
-	my $query = $results->[0]->query();
-	my @query_properties = $query =~ m{((?:QProp\.RESULTSET_|QProp\.ERROR_|QProp\.QUERY_).*?)[^A-Z_0-9]}sog;
+    my $query = $results->[0]->query();
+    my @query_properties = $query =~ m{((?:QProp\.RESULTSET_|QProp\.ERROR_|QProp\.QUERY_).*?)[^A-Z_0-9]}sog;
 
-	return STATUS_WONT_HANDLE if $#query_properties == -1;
+    return STATUS_WONT_HANDLE if $#query_properties == -1;
 
-	my $query_status = STATUS_OK;
+    my $query_status = STATUS_OK;
 
-	foreach my $result (@$results) {
+    foreach my $result (@$results) {
         my @error_codes = ();
-		my $property_status;
-		foreach my $query_property (@query_properties) {
+        my $property_status;
+        foreach my $query_property (@query_properties) {
             $property_status = STATUS_OK;
-			if (exists $properties{$query_property}) {
-				#
-				# This is a named property, call the respective validation procedure
-				#
-				$property_status = $validator->$query_property($result);
+            if (exists $properties{$query_property}) {
+                #
+                # This is a named property, call the respective validation procedure
+                #
+                $property_status = $validator->$query_property($result);
                 if ($property_status != STATUS_OK) {
                     say("ERROR: Query: $query does not have the required property: $query_property");
                 }
                 $query_status = $property_status if $property_status > $query_status;
-			} elsif (my ($error) = $query_property =~ m{ERROR_(.*)}so) {
-				#
-				# This is an error code, check that the query returned one of the given error codes
-				#
+            } elsif (my ($error) = $query_property =~ m{ERROR_(.*)}so) {
+                #
+                # This is an error code, check that the query returned one of the given error codes
+                #
                 if ($error !~ m{^\d*$}) {
-					say("ERROR: Query: $query needs to use a numeric code in in query property $query_property.");
-					return STATUS_ENVIRONMENT_FAILURE;
-				} 
+                    say("ERROR: Query: $query needs to use a numeric code in in query property $query_property.");
+                    return STATUS_ENVIRONMENT_FAILURE;
+                } 
                 push @error_codes, $error;
-			}
-		}
+            }
+        }
         my $err = $result->err();
         if ($err and scalar(@error_codes)) {
             $property_status = STATUS_ERROR_MISMATCH;
@@ -91,83 +91,83 @@ sub validate {
                 $query_status = $property_status if $property_status > $query_status;
             }
         }
-	}
+    }
 
-	if ($query_status != STATUS_OK) {
-		print Dumper $results if rqg_debug();
-	}
-	
-	return $query_status;
+    if ($query_status != STATUS_OK) {
+        print Dumper $results if rqg_debug();
+    }
+    
+    return $query_status;
 }
 
 
 sub RESULTSET_HAS_SAME_DATA_IN_EVERY_ROW {
-	my ($validator, $result) = @_;
+    my ($validator, $result) = @_;
 
-	return STATUS_OK if not defined $result->data();
-	return STATUS_OK if $result->rows() < 2;
+    return STATUS_OK if not defined $result->data();
+    return STATUS_OK if $result->rows() < 2;
 
-	my %data_hash;
-	foreach my $row (@{$result->data()}) {
-		my $data_item = join('<field>', @{$row});
-		$data_hash{$data_item}++;
-	}
-	
-	if (keys(%data_hash) > 1) {
-		return STATUS_CONTENT_MISMATCH;
-	} else {
-		return STATUS_OK;
-	}
+    my %data_hash;
+    foreach my $row (@{$result->data()}) {
+        my $data_item = join('<field>', @{$row});
+        $data_hash{$data_item}++;
+    }
+    
+    if (keys(%data_hash) > 1) {
+        return STATUS_CONTENT_MISMATCH;
+    } else {
+        return STATUS_OK;
+    }
 }
 
 sub RESULTSET_HAS_ZERO_OR_ONE_ROWS {
-	my ($validator, $result) = @_;
-	
-	if ($result->rows() > 1) {
-		return STATUS_LENGTH_MISMATCH;
-	} else {
-		return STATUS_OK;
-	}
+    my ($validator, $result) = @_;
+    
+    if ($result->rows() > 1) {
+        return STATUS_LENGTH_MISMATCH;
+    } else {
+        return STATUS_OK;
+    }
 }
 
 sub RESULTSET_HAS_ONE_ROW {
-	my ($validator, $result) = @_;
-	
-	if ($result->rows() != 1) {
-		return STATUS_LENGTH_MISMATCH;
-	} else {
-		return STATUS_OK;
-	}
+    my ($validator, $result) = @_;
+    
+    if ($result->rows() != 1) {
+        return STATUS_LENGTH_MISMATCH;
+    } else {
+        return STATUS_OK;
+    }
 }
 
 sub RESULTSET_IS_SINGLE_INTEGER_ONE {
-	my ($validator, $result) = @_;
-	
-	if (
-		(not defined $result->data()) ||
-		($#{$result->data()} != 0) ||
-		($result->rows() != 1) ||
-		($#{$result->data()->[0]} != 0) ||
-		($result->data()->[0]->[0] != 1)
-	) {
-		return STATUS_CONTENT_MISMATCH;
-	} else {
-		return STATUS_OK;
-	}
+    my ($validator, $result) = @_;
+    
+    if (
+        (not defined $result->data()) ||
+        ($#{$result->data()} != 0) ||
+        ($result->rows() != 1) ||
+        ($#{$result->data()->[0]} != 0) ||
+        ($result->data()->[0]->[0] != 1)
+    ) {
+        return STATUS_CONTENT_MISMATCH;
+    } else {
+        return STATUS_OK;
+    }
 }
 
 sub QUERY_IS_REPLICATION_SAFE {
 
-	my ($validator, $result) = @_;
+    my ($validator, $result) = @_;
 
-	my $warnings = $result->warnings();
+    my $warnings = $result->warnings();
 
-	if (defined $warnings) {
-		foreach my $warning (@$warnings) {
-			return STATUS_ENVIRONMENT_FAILURE if $warning->[1] == 1592;
-		}
-	}
-	return STATUS_OK;
+    if (defined $warnings) {
+        foreach my $warning (@$warnings) {
+            return STATUS_ENVIRONMENT_FAILURE if $warning->[1] == 1592;
+        }
+    }
+    return STATUS_OK;
 }
 
 1;
