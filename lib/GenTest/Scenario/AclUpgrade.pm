23f517b6dba9c7b542e279d6ab9b1b24c578d6ab
Elena Stepanova <elenst@montyprogram.com>
2019-01-16 02:12:52 +0200

    Upgrade scenario for ACL

---
+++ lib/GenTest/Scenario/AclUpgrade.pm
@@ -0,0 +1,368 @@
+# Copyright (C) 2017, 2018 MariaDB Corporation Ab
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+
+########################################################################
+#
+# The module implements a normal upgrade scenario with focus on ACL.
+#
+# The difference with the usual Upgrade test is that instead of
+# comparing the raw database dumps before and after upgrade,
+# we will compare the output of SHOW GRANT statements and such.
+# There is no point comparing the structure and contents of system
+# tables, we already know it will most likely differ; but the outcome
+# should be generally the same
+#
+########################################################################
+
+package GenTest::Scenario::AclUpgrade;
+
+require Exporter;
+@ISA = qw(GenTest::Scenario);
+
+use strict;
+use DBI;
+use GenTest;
+use GenTest::App::GenTest;
+use GenTest::Properties;
+use GenTest::Constants;
+use GenTest::Scenario;
+use Data::Dumper;
+use File::Copy;
+use File::Compare;
+
+use DBServer::MySQL::MySQLd;
+
+sub new {
+  my $class= shift;
+  my $self= $class->SUPER::new(@_);
+
+  if (!defined ${$self->getProperty('basedir')}[2] or (${$self->getProperty('basedir')}[1] eq ${$self->getProperty('basedir')}[2])) {
+    $self->printTitle('ACL - normal restart');
+  }
+  else {
+    $self->printTitle('ACL - normal upgrade/downgrade');
+  }
+
+  if (not defined $self->getProperty('grammar')) {
+    $self->setProperty('grammar', 'conf/mariadb/modules/acl.yy');
+  }
+  if (not defined $self->getProperty('threads')) {
+    $self->setProperty('threads', 4);
+  }
+  
+  return $self;
+}
+
+sub run {
+  my $self= shift;
+  my ($status, $old_server, $new_server, $gentest);
+  my ($old_user_definitions, $new_user_definitions, $old_grants, $new_grants);
+
+  $status= STATUS_OK;
+
+  # We can initialize both servers right away, because the second one
+  # runs with start_dirty, so it won't bootstrap
+  
+  $old_server= $self->prepareServer(1,
+    {
+      vardir => ${$self->getProperty('vardir')}[0],
+      port => ${$self->getProperty('port')}[0],
+      valgrind => 0,
+    }
+  );
+  $new_server= $self->prepareServer(2,
+    {
+      vardir => ${$self->getProperty('vardir')}[0],
+      port => ${$self->getProperty('port')}[0],
+      start_dirty => 1
+    }
+  );
+
+  say("-- Old server info: --");
+  say($old_server->version());
+  $old_server->printServerOptions();
+  say("-- New server info: --");
+  say($new_server->version());
+  $new_server->printServerOptions();
+  say("----------------------");
+
+  #####
+  $self->printStep("Starting the old server");
+
+  $status= $old_server->startServer;
+
+  if ($status != STATUS_OK) {
+    sayError("Old server failed to start");
+    return $self->finalize(STATUS_TEST_FAILURE,[]);
+  }
+  
+  #####
+  $self->printStep("Running test flow on the old server");
+
+  $gentest= $self->prepareGentest(1,
+    {
+      duration => int($self->getTestDuration * 2 / 3),
+      dsn => [$old_server->dsn($self->getProperty('database'))],
+      servers => [$old_server],
+    }
+  );
+
+  $status= $gentest->run();
+  
+  if ($status != STATUS_OK) {
+    sayError("Test flow on the old server failed");
+    return $self->finalize(STATUS_TEST_FAILURE,[$old_server]);
+  }
+
+  #####
+  $self->printStep("Getting ACL info from the old server");
+  
+  ($status, $old_user_definitions, $old_grants)= $self->collectAclData($old_server);
+  
+  if ($status != STATUS_OK) {
+    sayError("ACL info collection from the old server failed");
+    $status= STATUS_TEST_FAILURE if $status < STATUS_TEST_FAILURE;
+    return $self->finalize($status,[$old_server]);
+  }
+   
+  #####
+  $self->printStep("Stopping the old server");
+
+  $status= $old_server->stopServer;
+
+  if ($status != STATUS_OK) {
+    sayError("Shutdown of the old server failed");
+    return $self->finalize(STATUS_TEST_FAILURE,[$old_server]);
+  }
+
+  #####
+  $self->printStep("Checking the old server log for fatal errors after shutdown");
+
+  $status= $self->checkErrorLog($old_server, {CrashOnly => 1});
+
+  if ($status != STATUS_OK) {
+    sayError("Found fatal errors in the log, old server shutdown has apparently failed");
+    return $self->finalize(STATUS_TEST_FAILURE,[$old_server]);
+  }
+
+  #####
+  $self->printStep("Backing up data directory from the old server");
+
+  $old_server->backupDatadir($old_server->datadir."_orig");
+  move($old_server->errorlog, $old_server->errorlog.'_orig');
+
+  #####
+  $self->printStep("Starting the new server");
+
+  $status= $new_server->startServer;
+
+  if ($status != STATUS_OK) {
+    sayError("New server failed to start");
+    # Error log might indicate known bugs which will affect the exit code
+    $status= $self->checkErrorLog($new_server);
+    # ... but even if it's a known error, we cannot proceed without the server
+    return $self->finalize($status,[$new_server]);
+  }
+
+  #####
+  $self->printStep("Checking the server error log for errors after upgrade");
+
+  $status= $self->checkErrorLog($new_server);
+
+  if ($status != STATUS_OK) {
+    # Error log can show known errors. We want to update
+    # the global status, but don't want to exit prematurely
+    $self->setStatus($status);
+    sayError("Found errors in the log after upgrade");
+    if ($status > STATUS_CUSTOM_OUTCOME) {
+      return $self->finalize(STATUS_UPGRADE_FAILURE,[$new_server]);
+    }
+  }
+
+  #####
+  if ($old_server->majorVersion ne $new_server->majorVersion) {
+    $self->printStep("Running mysql_upgrade");
+    $status= $new_server->upgradeDb;
+    if ($status != STATUS_OK) {
+      sayError("mysql_upgrade failed");
+      return $self->finalize(STATUS_UPGRADE_FAILURE,[$new_server]);
+    }
+  }
+  else {
+    $self->printStep("mysql_upgrade is skipped, as servers have the same major version");
+  }
+
+  #####
+  $self->printStep("Checking the database state after upgrade");
+
+  $status= $new_server->checkDatabaseIntegrity;
+
+  if ($status != STATUS_OK) {
+    sayError("Database appears to be corrupt after upgrade");
+    return $self->finalize(STATUS_UPGRADE_FAILURE,[$new_server]);
+  }
+  
+  #####
+  $self->printStep("Getting ACL info from the new server");
+  
+  ($status, $new_user_definitions, $new_grants)= $self->collectAclData($new_server);
+  
+  if ($status != STATUS_OK) {
+    sayError("ACL info collection from the new server failed");
+    $status= STATUS_UPGRADE_FAILURE if $status < STATUS_UPGRADE_FAILURE;
+    return $self->finalize($status,[$new_server]);
+  }
+
+  #####
+  $self->printStep("Comparing ACL data before and after upgrade");
+  
+  foreach my $u (sort keys %$old_grants) {
+    if (not exists $new_grants->{$u}) {
+      sayError("User/role $u disappeared from the user table after upgrade");
+      $status= STATUS_UPGRADE_FAILURE;
+    }
+    elsif ($new_grants->{$u} ne $old_grants->{$u}) {
+      sayError("Grants for user/role $u changed after upgrade:\nOld: $old_grants->{$u}\nNew: $new_grants->{$u}");
+      $status= STATUS_UPGRADE_FAILURE;
+    }
+  }
+      
+  foreach my $u (sort keys %$new_grants) {
+    if (not exists $old_grants->{$u}) {
+      sayError("User/role $u appeared in the user table after upgrade");
+      $status= STATUS_UPGRADE_FAILURE;
+    }
+  }
+
+  foreach my $u (sort keys %$old_user_definitions) {
+    if ($new_user_definitions->{$u} ne $old_user_definitions->{$u}) {
+      sayError("User definition for $u changed after upgrade:\nOld: $old_user_definitions->{$u}\nNew: $new_user_definitions->{$u}");
+      $status= STATUS_UPGRADE_FAILURE;
+    }
+  }
+  
+  if ($status == STATUS_OK) {
+    say("Comparison didn't reveal any discrepancies");
+  } else {
+    return $self->finalize(STATUS_UPGRADE_FAILURE,[$new_server]);
+  }
+  
+  #####
+  $self->printStep("Running test flow on the new server");
+
+  $gentest= $self->prepareGentest(2,
+    {
+      duration => int($self->getTestDuration / 3),
+      dsn => [$new_server->dsn($self->getProperty('database'))],
+      servers => [$new_server],
+      'start-dirty' => 1,
+    },
+    my $skip_gendata=1
+  );
+  $status= $gentest->run();
+
+  if ($status != STATUS_OK) {
+    sayError("Test flow on the new server failed");
+    #####
+    $self->printStep("Checking the server error log for known errors");
+
+    if ($self->checkErrorLog($new_server) == STATUS_CUSTOM_OUTCOME) {
+      $status= STATUS_CUSTOM_OUTCOME;
+    }
+
+    $self->setStatus($status);
+    return $self->finalize($status,[$new_server])
+  }
+
+  #####
+  $self->printStep("Stopping the new server");
+
+  $status= $new_server->stopServer;
+
+  if ($status != STATUS_OK) {
+    sayError("Shutdown of the new server failed");
+    return $self->finalize(STATUS_UPGRADE_FAILURE,[$new_server]);
+  }
+
+  return $self->finalize($status,[]);
+}
+
+sub collectAclData {
+  my ($self, $server)= @_;
+  my $res= STATUS_OK;
+
+  say("Collecting user definitions");
+  my $dbh= $server->dbh;
+
+  my %user_definitions= ();
+  # MDEV-17950: Skip users identified via not installed plugins
+  $dbh->do("FLUSH PRIVILEGES");
+  my $users= $dbh->selectcol_arrayref("SELECT CONCAT('`',user,'`','\@','`',host,'`') FROM mysql.user WHERE is_role = 'N' AND ( plugin = '' or plugin in (select plugin_name from information_schema.plugins where plugin_status = 'ACTIVE'))");
+  if ($dbh->err() > 0) {
+    sayError("Couldn't fetch users, error: ".$dbh->err()." (".$dbh->errstr().")");
+    $users= [];
+    $res= STATUS_DATABASE_CORRUPTION;
+  }
+  else {
+    say("Found ".scalar(@$users)." users");
+    foreach my $u (@$users) {
+      my $sth= $dbh->prepare("SHOW CREATE USER $u");
+      $sth->execute;
+      if ($sth->err() > 0) {
+        sayError("Couldn't fetch user definition for $u, error: ".$sth->err()." (".$sth->errstr().")");
+        $res= STATUS_TEST_FAILURE;
+      }
+      else {
+        my $def= $sth->fetchrow_arrayref;
+        $user_definitions{$u}= $def->[0];
+        # MDEV-18263: Inconsistent appearance of CIPHER ''
+        $user_definitions{$u} =~ s/\s+CIPHER\s+''//gs;
+      }
+    }
+  }
+  my $roles= $dbh->selectcol_arrayref("SELECT CONCAT('`',user,'`') FROM mysql.user WHERE is_role = 'Y'");
+  if ($dbh->err() > 0) {
+    sayError("Couldn't fetch roles, error: ".$dbh->err()." (".$dbh->errstr().")");
+    $roles= [];
+    $res= STATUS_DATABASE_CORRUPTION;
+  }
+  else {
+    say("Found ".scalar(@$roles)." roles");
+  }
+  say("Collecting grants");
+
+  my %grants= ();
+  foreach my $u (@$users, @$roles) {
+    my $sth= $dbh->prepare("SHOW GRANTS FOR $u");
+    $sth->execute;
+    if ($sth->err() > 0) {
+      sayError("Couldn't fetch grants for $u, error: ".$sth->err()." (".$sth->errstr().")");
+      $res= STATUS_TEST_FAILURE if $res < STATUS_TEST_FAILURE;
+    }
+    else {
+      my $def= $sth->fetchrow_arrayref;
+      $grants{$u}= $def->[0];
+      # MDEV-18263: Inconsistent appearance of CIPHER ''
+      $grants{$u} =~ s/\s+CIPHER\s+''//gs;
+    }
+  }
+  return ($res, \%user_definitions, \%grants);
+}
+
+
+
+1;
