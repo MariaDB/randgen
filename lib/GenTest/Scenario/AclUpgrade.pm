19e81706fbfffc28d2f71d2c22b361ecd4593d9a
Elena Stepanova <elenst@montyprogram.com>
2019-01-18 17:10:01 +0200

    Fixed typo in query

--- lib/GenTest/Scenario/AclUpgrade.pm
+++ lib/GenTest/Scenario/AclUpgrade.pm
@@ -330,7 +330,7 @@ sub collectAclData {
 
   # MDEV-17950: Skip users identified via not installed plugins
   # MDEV-18290: Skip users identified via ed25519 which have non-empty authentication_string
-  my $query= "SELECT CONCAT('`',user,'`','\@','`',host,'`') FROM mysql.user WHERE ( plugin = '' or plugin in (select plugin_name from information_schema.plugins where plugin_status = 'ACTIVE') and (plugin != 'ed25519' or authentication_string == '') )";
+  my $query= "SELECT CONCAT('`',user,'`','\@','`',host,'`') FROM mysql.user WHERE ( plugin = '' or plugin in (select plugin_name from information_schema.plugins where plugin_status = 'ACTIVE') and (plugin != 'ed25519' or authentication_string = '') )";
 
   if ($has_roles) {
     $query.= " AND is_role = 'N'";
