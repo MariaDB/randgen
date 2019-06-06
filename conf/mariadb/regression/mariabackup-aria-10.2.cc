895d2ca001728b2ba0941aa47e4ca88e657e977f
Elena Stepanova <elenst@mariadb.com>
2019-06-06 03:44:27 +0300

    Mariabackup combinations

---
+++ conf/mariadb/regression/mariabackup-aria-10.2.cc
@@ -0,0 +1,29 @@
+our ($encryption, $mariabackup_scenarios);
+require 'conf/mariadb/include/encryption_on_off';
+require 'conf/mariadb/include/mariabackup.scenarios';
+
+$combinations = [
+  [
+  '
+  --duration=350
+  --threads=4
+  --seed=time
+  --reporters=Backtrace,ErrorLog,Deadlock
+  --skip-gendata
+  --gendata-advanced
+  --engine=Aria
+  --views
+  --grammar=conf/mariadb/generic-dml.yy
+  --redefine=conf/mariadb/bulk_insert.yy
+  --filter=conf/mariadb/10.4-combo-filter.ff
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--loose-innodb-lock-wait-timeout=5
+  --mysqld=--loose-debug_assert_on_not_freed_memory=0
+  '],
+  # Encryption
+    $encryption,
+  # Scenarios (DML-only, DDL+DML)
+    $mariabackup_scenarios,
+];
---
+++ conf/mariadb/regression/mariabackup-innodb-10.2.cc
@@ -0,0 +1,35 @@
+our ($encryption, $mariabackup_scenarios, $innodb_pagesizes, $innodb_compression);
+require 'conf/mariadb/include/encryption_on_off';
+require 'conf/mariadb/include/mariabackup.scenarios';
+require 'conf/mariadb/include/innodb_pagesize';
+require 'conf/mariadb/include/innodb_compression';
+
+$combinations = [
+  [
+  '
+  --duration=350
+  --threads=4
+  --seed=time
+  --reporters=Backtrace,ErrorLog,Deadlock
+  --skip-gendata
+  --gendata-advanced
+  --engine=InnoDB
+  --views
+  --grammar=conf/mariadb/generic-dml.yy
+  --redefine=conf/mariadb/bulk_insert.yy
+  --filter=conf/mariadb/10.4-combo-filter.ff
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--loose-innodb-lock-wait-timeout=5
+  --mysqld=--loose-debug_assert_on_not_freed_memory=0
+  '],
+  # Compression
+    $innodb_compression,
+  # Pagesize
+    $innodb_pagesizes,
+  # Encryption
+    $encryption,
+  # Scenarios (DML-only, DDL+DML)
+    $mariabackup_scenarios,
+];
