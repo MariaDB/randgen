f85b27857275ec8787c9c12f0bf88454f294ca6e
Elena Stepanova <elenst@mariadb.com>
2019-06-07 22:29:29 +0300

    Integration tests

---
+++ conf/mariadb/regression/combo.cc
@@ -0,0 +1,37 @@
+our ($encryption, $grammars);
+require 'conf/mariadb/include/encryption_on_off';
+require 'conf/mariadb/include/combo.grammars';
+
+$combinations = [
+  [
+  '
+  --threads=6
+  --duration=350
+  --no-mask
+  --seed=time
+  --reporters=Backtrace,ErrorLog,Deadlock
+  --validators=TransformerNoComparator
+  --transformers=ExecuteAsCTE,ExecuteAsExecuteImmediate,ExecuteAsDeleteReturning,ExecuteAsInsertSelect,ExecuteAsUnion,ExecuteAsUpdateDelete,ExecuteAsView,ExecuteAsPreparedTwice
+  --views
+  --redefine=conf/mariadb/bulk_insert.yy
+  --redefine=conf/mariadb/alter_table.yy
+  --redefine=conf/mariadb/sp.yy
+  --redefine=conf/mariadb/modules/foreign_keys.yy
+  --redefine=conf/mariadb/modules/admin.yy
+  --redefine=conf/mariadb/modules/sql_mode.yy
+  --mysqld=--server-id=111
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--innodb-lock-wait-timeout=5
+  '],
+  # Combo
+    $grammars,
+  # Encryption
+    $encryption,
+  [
+    '',
+    '--mysqld=--log-bin --mysqld=--log_bin_trust_function_creators=1',
+    '--mysqld=--log-bin --mysqld=--log_bin_trust_function_creators=1 --mysqld=--binlog-format=row',
+  ]
+];
