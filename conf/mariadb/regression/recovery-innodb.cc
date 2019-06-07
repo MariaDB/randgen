f12306cb0775c4a30fdc7f51221fa6176cb1dec9
Elena Stepanova <elenst@mariadb.com>
2019-06-07 17:00:02 +0300

    InnoDB recovery tests

---
+++ conf/mariadb/regression/recovery-innodb.cc
@@ -0,0 +1,32 @@
+our ($encryption, $recovery_scenarios, $innodb_pagesizes, $innodb_compression);
+require 'conf/mariadb/include/encryption_on_off';
+require 'conf/mariadb/include/recovery.scenarios';
+require 'conf/mariadb/include/innodb_pagesize';
+require 'conf/mariadb/include/innodb_compression';
+
+$combinations = [
+  [
+  '
+  --threads=4
+  --no-mask
+  --seed=time
+  --grammar=conf/mariadb/oltp-transactional.yy
+  --grammar2=conf/mariadb/oltp_and_ddl.yy
+  --gendata=conf/mariadb/innodb_upgrade.zz
+  --gendata=conf/mariadb/innodb_upgrade_compression.zz
+  --gendata-advanced
+  --mysqld=--server-id=111
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--innodb-lock-wait-timeout=5
+  '],
+  # Compression
+    $innodb_compression,
+  # Pagesize
+    $innodb_pagesizes,
+  # Encryption
+    $encryption,
+  # Scenarios
+    $recovery_scenarios,
+];
