3aa1aebc98fee1052b4cb160ebbe98f35ed272a1
Elena Stepanova <elenst@mariadb.com>
2019-06-07 20:19:45 +0300

    Aria recovery tests

---
+++ conf/mariadb/regression/recovery-aria.cc
@@ -0,0 +1,35 @@
+our ($encryption);
+require 'conf/mariadb/include/encryption_on_off';
+
+$combinations = [
+  [
+  '
+  --threads=6
+  --duration=250
+  --no-mask
+  --engine=Aria
+  --seed=time
+  --redefine=conf/mariadb/bulk_insert.yy
+  --redefine2=conf/mariadb/alter_table.yy
+  --scenario=CrashUpgrade
+  --reporters=Backtrace,ErrorLog,Deadlock
+  --mysqld=--server-id=111
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--innodb-lock-wait-timeout=5
+  '],
+  [
+    '--grammar=conf/mariadb/oltp-transactional.yy --gendata=conf/mariadb/oltp-aria.zz',
+    '--grammar=conf/mariadb/oltp.yy --gendata=conf/mariadb/oltp-aria.zz',
+    '--grammar=conf/mariadb/generic-dml.yy --gendata=conf/mariadb/oltp-aria.zz',
+    '--grammar=conf/engines/engine_stress.yy --gendata=conf/engines/engine_stress.zz',
+  ],
+  [
+    '--gendata-advanced --vcols --default-storage-engine=Aria',
+    '--mysqld=--log_bin_trust_function_creators=1 --mysqld=--log-bin',
+    '--mysqld=--aria_group_commit=hard --mysqld=--aria_group_commit_interval=1000000',
+  ],
+  # Encryption
+    $encryption,
+];
