0564ffa128656c35fa0f08c02e7497a2ea96a5a1
Elena Stepanova <elenst@mariadb.com>
2019-07-31 03:30:25 +0300

    10.4 configuration

---
+++ conf/mariadb/regression/mariabackup-other-engines-10.4.cc
@@ -0,0 +1,34 @@
+our $mariabackup_scenarios;
+require 'conf/mariadb/include/mariabackup-10.4.scenarios';
+
+$combinations = [
+  [
+  '
+  --duration=350
+  --threads=4
+  --seed=time
+  --reporters=Backtrace,ErrorLog,Deadlock
+  --skip-gendata
+  --gendata-advanced
+  --views
+  --grammar=conf/mariadb/generic-dml.yy
+  --redefine=conf/mariadb/bulk_insert.yy
+  --filter=conf/mariadb/10.4-combo-filter.ff
+  --mysqld=--log_output=FILE
+  --mysqld=--max-statement-time=20
+  --mysqld=--lock-wait-timeout=10
+  --mysqld=--loose-innodb-lock-wait-timeout=5
+  --mysqld=--loose-debug_assert_on_not_freed_memory=0
+  '],
+  # Engines
+  [
+    '--engine=MyISAM',
+    '
+    --engine=RocksDB
+    --mysqld=--plugin-load-add=ha_rocksdb
+    --mysqld=--binlog-format=row
+    ',
+  ],
+  # Scenarios (DML-only, DDL+DML)
+    $mariabackup_scenarios,
+];
