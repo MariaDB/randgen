aeaaf52a8c497d1bf00a3453163e65de17efc206
Elena Stepanova <elenst@mariadb.com>
2019-11-21 21:45:06 +0200

    Grammar and command line for MDEV-21120

---
+++ mdev21120.cmd
@@ -0,0 +1,1 @@
+perl ./runall-new.pl --grammar=mdev21120.yy --duration=350 --threads=1 --skip-gendata --scenario=Restart --scenario-shutdown-timeout=600 --mysqld=--log-bin --vardir=/dev/shm/vardir_mdev21120 --basedir=$1
