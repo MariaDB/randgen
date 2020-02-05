7cdebc365da281837cc217f7e43e3e05e1945822
Elena Stepanova <elenst@mariadb.com>
2020-02-06 00:37:31 +0200

    More careful processing for command-line and grammar simplification

--- util/simplify-rqg-test.pl
+++ util/simplify-rqg-test.pl
@@ -42,12 +42,14 @@ my $storage_prefix;
 my $mtr_thread= 500;
 my $expected_output;
 my $seed= 1;
+my $skip_cmd_simplification= 0;
 
 GetOptions(
     'workdir=s' => \$storage_prefix,
     'mtr_thread|mtr-thread=i' => \$mtr_thread,
     'output=s' => \$expected_output,
     'seed=i' => \$seed,
+    'skip-cmd' => \$skip_cmd_simplification,
 );
 
 unless (defined $storage_prefix) {
@@ -77,188 +79,187 @@ my $grammar;
 
 my $iteration= 0;
 
-my $rqgcmd_base =
-    "perl runall-trials.pl ".
-    "--vardir=$vardir ".
-    "--mtr-build-thread=$mtr_thread ".
-    "--seed=$seed "
-;
+my $mandatory_options= '';
 
-$rqgcmd_base.= " --output=\"$expected_output\"" if $expected_output;
-
-foreach my $opt (@ARGV)
-{
-    # Some options will be kept as is
-    if ($opt =~ /^--grammar=(.*)/) {
-        $grammar= $1;
-    } elsif ($opt =~ /^--redefine=(.*)/) {
-        push @grammars, $1;
-    } elsif ($opt =~ /^(--mysqld\d?)=--(.*)/) {
-        my ($left, $right)= ($1, $2);
-        if ($right !~ /^loose-/) {
-            $right= 'loose-'.$right;
+if ($skip_cmd_simplification) {
+    foreach my $opt (@ARGV)
+    {
+        # Some options will be kept as is
+        if ($opt =~ /^--grammar=(.*)/) {
+            $grammar= $1;
+        } elsif ($opt =~ /^--redefine=(.*)/) {
+            push @grammars, $1;
+        } elsif ($opt =~ /^--(vardir\d?|mtr[-_]build[-_]thread)=/) {
+            # vardir and mtr-build-thread are overridden anyway, skipping them completely
+        } else {
+            $mandatory_options.= " $opt";
         }
-        push @simplifiable_options, "$left=--$right";
-    } elsif ($opt =~ /^--(basedir\d?|duration|queries|threads|exit[-_]status|trials)=/) {
-        $rqgcmd_base.= " $opt";
-    } elsif ($opt =~ /^--(vardir\d?|seed|mtr[-_]build[-_]thread)=/) {
-        # vardir, mtr-build-thread and seed are overridden anyway, skipping them completely
-    } elsif ($opt =~ /--transformers=(.*)/) {
-        my @vals= split /,/, $1;
-        @transformers= (@transformers, @vals);
-    } elsif ($opt =~ /--validators=(.*)/) {
-        my @vals= split /,/, $1;
-        foreach my $v (@vals) {
-            if ($v =~ /Transformer/) {
-                push @transformation_validators, $v;
-            } else {
-                push @simplifiable_options, "--validators=$v";
-            }
-        }
-    } elsif ($opt =~ /--reporters=(.*)/) {
-        my @vals= split /,/, $1;
-        foreach my $v (@vals) {
-            if ($v eq 'Deadlock') {
-                $rqgcmd_base.= " --reporters=$v";
-            } else {
-                push @simplifiable_options, "--reporters=$v";
-            }
-        }
-    } else {
-        push @simplifiable_options, $opt;
     }
+    say("Command line simplification is skipped upon request, proceeding to grammar simplification");
+} else {
+    foreach my $opt (@ARGV)
+    {
+        # Some options will be kept as is
+        if ($opt =~ /^--grammar=(.*)/) {
+            $grammar= $1;
+        } elsif ($opt =~ /^--redefine=(.*)/) {
+            push @grammars, $1;
+        } elsif ($opt =~ /^(--mysqld\d?)=--(.*)/) {
+            my ($left, $right)= ($1, $2);
+            if ($right !~ /^loose-/) {
+                $right= 'loose-'.$right;
+            }
+            push @simplifiable_options, "$left=--$right";
+        } elsif ($opt =~ /^--(basedir\d?|duration|queries|threads|exit[-_]status|trials)=/) {
+            $mandatory_options.= " $opt";
+        } elsif ($opt =~ /^--(vardir\d?|mtr[-_]build[-_]thread)=/) {
+            # vardir and mtr-build-thread are overridden anyway, skipping them completely
+        } elsif ($opt =~ /--transformers=(.*)/) {
+            my @vals= split /,/, $1;
+            @transformers= (@transformers, @vals);
+        } elsif ($opt =~ /--validators=(.*)/) {
+            my @vals= split /,/, $1;
+            foreach my $v (@vals) {
+                if ($v =~ /Transformer/) {
+                    push @transformation_validators, $v;
+                } else {
+                    push @simplifiable_options, "--validators=$v";
+                }
+            }
+        } elsif ($opt =~ /--reporters=(.*)/) {
+            my @vals= split /,/, $1;
+            foreach my $v (@vals) {
+                if ($v eq 'Deadlock') {
+                    $mandatory_options.= " --reporters=$v";
+                } else {
+                    push @simplifiable_options, "--reporters=$v";
+                }
+            }
+        } else {
+            push @simplifiable_options, $opt;
+        }
+    }
+
+    say("###########################");
+    say("### Running initial trials");
+    say("");
+
+    my $options= $mandatory_options;
+    map { $options.= " --transformers=$_" } @transformers if scalar(@transformers);
+    map { $options.= " --validators=$_" } @transformation_validators if scalar(@transformation_validators);
+    map { $options.= " --redefine=$_" } @grammars if scalar(@grammars);
+    $options.= " @simplifiable_options";
+    # runall-trials returns 1 if the failure was reproduced, and 0 otherwise
+    my $res= run_trials($options, $grammar, $iteration);
+    if ($res) {
+        say("###### SUCCESS with cmd $iteration: initial run reproduced the issue");
+        $iteration++;
+    } else {
+        say("Initial run failed to reproduce the issue, giving up");
+        exit 1;
+    }
+
+    say("########################################");
+    say("### Running command line simplification");
+    say("");
+
+    # At this point @grammars contains redefines, if any,
+    # and $grammar contains the main grammar
+
+    if (scalar @grammars)
+    {
+        say("Redefines to be simplified: @grammars");
+
+        my @preserved_redefines= ();
+        my $options= $mandatory_options;
+        map { $options.= " --transformers=$_" } @transformers if scalar(@transformers);
+        map { $options.= " --validators=$_" } @transformation_validators if scalar(@transformation_validators);
+        $options.= " @simplifiable_options";
+
+        for (my $i=0; $i<=$#grammars; $i++) {
+            say("-----");
+            say("Trying to remove redefine $grammars[$i]");
+            my @new_grammars= ($i < $#grammars ? (@preserved_redefines, @grammars[$i+1..$#grammars]) : (@preserved_redefines));
+            my $temp_options= $options;
+            map { $temp_options.= " --redefine=$_" } @new_grammars if scalar(@new_grammars);
+            my $res= run_trials($temp_options, $grammar, $iteration);
+            if ($res) {
+                say("###### SUCCESS with cmd $iteration: redefine $grammars[$i] can be removed");
+            } else {
+                say("Redefine $grammars[$i] has to be preserved");
+                push @preserved_redefines, $grammars[$i];
+            }
+            $iteration++;
+        }
+        @grammars= @preserved_redefines;
+    }
+
+    if (scalar @transformers)
+    {
+        say("-----");
+        say("Transformers to be simplified: @transformers");
+        say("Transformation validators to be taken into account: @transformation_validators");
+
+        my @preserved_transformers= ();
+        my $options= $mandatory_options;
+        map { $options.= " --redefine=$_" } @grammars if scalar(@grammars);
+        $options.= " @simplifiable_options";
+
+        for (my $i=0; $i<=$#transformers; $i++) {
+            say("-----");
+            say("Trying to remove transformer $transformers[$i]");
+            my @new_transformers= ($i < $#transformers ? (@preserved_transformers, @transformers[$i+1..$#transformers]) : (@preserved_transformers));
+            my $temp_options= $options;
+            map { $temp_options.= " --transformers=$_" } @new_transformers if scalar(@new_transformers);
+            map { $temp_options.= " --validators=$_" } @transformation_validators if scalar(@new_transformers);
+            my $res= run_trials($temp_options, $grammar, $iteration);
+            if ($res) {
+                say("###### SUCCESS with cmd $iteration: transformer $transformers[$i] can be removed");
+            } else {
+                say("Transformer $transformers[$i] has to be preserved");
+                push @preserved_transformers, $transformers[$i];
+            }
+            $iteration++;
+        }
+        if (scalar @preserved_transformers) {
+            map { $mandatory_options.= " --transformers=$_" } @preserved_transformers;
+            map { $mandatory_options.= " --validators=$_" } @transformation_validators;
+        }
+    }
+
+    if (scalar @simplifiable_options)
+    {
+        say("-----");
+        say("Options to be simplified: @simplifiable_options");
+
+        my @preserved_options= ();
+        my $options= $mandatory_options;
+        map { $options.= " --redefine=$_" } @grammars if scalar(@grammars);
+
+        for (my $i=0; $i<=$#simplifiable_options; $i++) {
+            say("-----");
+            say("Trying to remove option $simplifiable_options[$i]");
+            my $temp_options= "$options @preserved_options @simplifiable_options[$i+1..$#simplifiable_options]";
+            my $res= run_trials($temp_options, $grammar, $iteration);
+            if ($res) {
+                say ("###### SUCCESS with cmd $iteration: option $simplifiable_options[$i] can be removed ######");
+            } else {
+                say("Option $simplifiable_options[$i] has to be preserved");
+                push @preserved_options, $simplifiable_options[$i];
+            }
+            $iteration++;
+        }
+        $mandatory_options.= " @preserved_options";
+    }
+
+    say("Command line simplification finished, final set of necessary options (excluding grammars):");
+    say("$mandatory_options --grammar=$grammar" . (scalar(@grammars) ? ' --redefine='.join ' --redefine=', @grammars : ''));
 }
 
 unless ($grammar) {
     say("No grammar defined");
     exit 1;
 }
-
-say("###########################");
-say("### Running initial trials");
-say("");
-
-my $cmd= "$rqgcmd_base --grammar=$grammar";
-map { $cmd.= " --transformers=$_" } @transformers if scalar(@transformers);
-map { $cmd.= " --validators=$_" } @transformation_validators if scalar(@transformation_validators);
-map { $cmd.= " --redefine=$_" } @grammars if scalar(@grammars);
-$cmd.= " @simplifiable_options";
-$cmd.= " > ${storage}/${iteration}.log";
-say($cmd);
-# runall-trials returns 1 if the failure was reproduced, and 0 otherwise
-my $res= system($cmd);
-if ($res) {
-    say("###### SUCCESS with cmd $iteration: initial run reproduced the issue");
-    $iteration++;
-} else {
-    say("Initial run failed to reproduce the issue, giving up");
-    exit 1;
-}
-
-say("########################################");
-say("### Running command line simplification");
-say("");
-
-# At this point @grammars contains redefines, if any,
-# and $grammar contains the main grammar
-
-if (scalar @grammars)
-{
-    say("Redefines to be simplified: @grammars");
-
-    my @preserved_redefines= ();
-    my $cmd= "$rqgcmd_base --grammar=$grammar";
-    map { $cmd.= " --transformers=$_" } @transformers if scalar(@transformers);
-    map { $cmd.= " --validators=$_" } @transformation_validators if scalar(@transformation_validators);
-    $cmd.= " @simplifiable_options";
-    
-    for (my $i=0; $i<=$#grammars; $i++) {
-        say("-----");
-        say("Trying to remove redefine $grammars[$i]");
-        my @new_grammars= ($i < $#grammars ? (@preserved_redefines, @grammars[$i+1..$#grammars]) : (@preserved_redefines));
-        my $new_cmd= $cmd;
-        map { $new_cmd.= " --redefine=$_" } @new_grammars if scalar(@new_grammars);
-        $new_cmd.= " > ${storage}/${iteration}.log";
-        say($new_cmd);
-        # runall-trials returns 1 if the failure was reproduced, and 0 otherwise
-        my $res= system($new_cmd);
-        if ($res) {
-            say("###### SUCCESS with cmd $iteration: redefine $grammars[$i] can be removed");
-        } else {
-            say("Redefine $grammars[$i] has to be preserved");
-            push @preserved_redefines, $grammars[$i];
-        }
-        $iteration++;
-    }
-    @grammars= @preserved_redefines;
-}
-
-if (scalar @transformers)
-{
-    say("-----");
-    say("Transformers to be simplified: @transformers");
-    say("Transformation validators to be taken into account: @transformation_validators");
-
-    my @preserved_transformers= ();
-    my $cmd= "$rqgcmd_base --grammar=$grammar";
-    map { $cmd.= " --redefine=$_" } @grammars if scalar(@grammars);
-    $cmd.= " @simplifiable_options";
-
-    for (my $i=0; $i<=$#transformers; $i++) {
-        say("-----");
-        say("Trying to remove transformer $transformers[$i]");
-        my @new_transformers= ($i < $#transformers ? (@preserved_transformers, @transformers[$i+1..$#transformers]) : (@preserved_transformers));
-        my $new_cmd= $cmd;
-        map { $new_cmd.= " --transformers=$_" } @new_transformers if scalar(@new_transformers);
-        map { $new_cmd.= " --validators=$_" } @transformation_validators if scalar(@new_transformers);
-        $new_cmd.= " > ${storage}/${iteration}.log";
-        say($new_cmd);
-        # runall-trials returns 1 if the failure was reproduced, and 0 otherwise
-        my $res= system($new_cmd);
-        if ($res) {
-            say("###### SUCCESS with cmd $iteration: transformer $transformers[$i] can be removed");
-        } else {
-            say("Transformer $transformers[$i] has to be preserved");
-            push @preserved_transformers, $transformers[$i];
-        }
-        $iteration++;
-    }
-    if (scalar @preserved_transformers) {
-        map { $rqgcmd_base.= " --transformers=$_" } @preserved_transformers;
-        map { $rqgcmd_base.= " --validators=$_" } @transformation_validators;
-    }
-}
-
-if (scalar @simplifiable_options)
-{
-    say("-----");
-    say("Options to be simplified: @simplifiable_options");
-
-    my @preserved_options= ();
-    my $cmd= "$rqgcmd_base --grammar=$grammar";
-    map { $cmd.= " --redefine=$_" } @grammars if scalar(@grammars);
-
-    for (my $i=0; $i<=$#simplifiable_options; $i++) {
-        say("-----");
-        say("Trying to remove option $simplifiable_options[$i]");
-        my $new_cmd= "$cmd @preserved_options @simplifiable_options[$i+1..$#simplifiable_options]";
-        $new_cmd.= " > ${storage}/${iteration}.log";
-        say($new_cmd);
-        # runall-trials returns 1 if the failure was reproduced, and 0 otherwise
-        my $res= system($new_cmd);
-        if ($res) {
-            say ("###### SUCCESS with cmd $iteration: option $simplifiable_options[$i] can be removed ######");
-        } else {
-            say("Option $simplifiable_options[$i] has to be preserved");
-            push @preserved_options, $simplifiable_options[$i];
-        }
-        $iteration++;
-    }
-    $rqgcmd_base.= " @preserved_options";
-}
-
-say("Command line simplification finished, final set of options (excluding grammars):");
-say("$rqgcmd_base");
 
 say("####################################");
 say("### Running grammar simplification");
@@ -290,20 +291,7 @@ my $simplifier = GenTest::Simplifier::Grammar->new(
         print GRAMMAR $oracle_grammar;
         close (GRAMMAR);
 
-        say("run_id = $run_id; iteration = $iteration");
-
-        my $current_rqg_log = $storage . '/' . $iteration . '.log';
-        my $start_time = Time::HiRes::time();
-
-        my $rqgcmd= "$rqgcmd_base --grammar=$current_grammar >$current_rqg_log 2>&1";
-        say($rqgcmd);
-        my $rqg_status = system($rqgcmd);
-        $rqg_status = $rqg_status >> 8;
-
-        my $end_time = Time::HiRes::time();
-        my $duration = $end_time - $start_time;
-
-        say("rqg_status = $rqg_status; duration = $duration");
+        my $rqg_status = run_trials($mandatory_options, $current_grammar, $iteration);
 
         # runall-trials.pl returns 1 if the failure was reproduced
         # and 0 otherwise
@@ -324,4 +312,20 @@ if (defined $simplified_grammar) {
     exit 0;
 } else {
     exit 1;
+}
+
+sub run_trials {
+    my ($options, $grammar, $iteration)= @_;
+    my $start_time = Time::HiRes::time();
+    my $cmd= "perl runall-trials.pl $options --grammar=$grammar --vardir=$vardir --mtr-build-thread=$mtr_thread --seed=$seed";
+    $cmd.= " --output=\"$expected_output\"" if $expected_output;
+    $cmd.= " > ${storage}/${iteration}.log";
+    say("run_id = $run_id; iteration = $iteration");
+    say($cmd);
+    my $res= system($cmd);
+    $res= $res >> 8;
+    my $end_time = Time::HiRes::time();
+    my $duration = $end_time - $start_time;
+    say("rqg_status = $res; duration = $duration");
+    return $res;
 }
