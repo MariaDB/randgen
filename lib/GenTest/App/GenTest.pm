db8406a2125d8430879dad175c51fe24519f3abe
Elena Stepanova <elenst@mariadb.com>
2020-05-17 19:36:09 +0300

    Re-cache metadata after first queries

--- lib/GenTest/App/GenTest.pm
+++ lib/GenTest/App/GenTest.pm
@@ -479,6 +479,10 @@ sub workerProcess {
 
     foreach my $i (1..$self->config->queries) {
         my $query_result = $mixer->next();
+        if ($i == 1) {
+            # Re-caching metadata after *_init queries, as they often create structures
+            $executors[0]->cacheMetaData(1);
+        }
         $worker_result = $query_result if $query_result > $worker_result && $query_result > STATUS_TEST_FAILURE;
 
         if ($query_result > STATUS_CRITICAL_FAILURE) {
