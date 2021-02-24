b4141f28327c72ee51db42445bfb449524744fbb
Elena Stepanova <elenst@mariadb.com>
2021-02-24 19:42:49 +0200

    Disable extra printing of top output

--- lib/GenTest/Reporter/MemoryUsage.pm
+++ lib/GenTest/Reporter/MemoryUsage.pm
@@ -85,7 +85,7 @@ sub get_top_output {
   my ($mem, $unit, $cpu);
   if (open(TOP, 'top -b -n 1 -p '.$pid.' |')) {
     while (<TOP>) {
-      say("MemoryUsage (line from top output): $_");
+#      say("MemoryUsage (line from top output): $_");
       # Skipping everything but the process for now, but may parse more in future
       next unless /^\s*$pid/;
       # PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND
