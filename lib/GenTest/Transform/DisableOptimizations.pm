22131b43e705608365ab7cc770ca06a2a6af8bd4
Elena Stepanova <elenst@mariadb.com>
2021-02-27 21:23:16 +0200

    Adjust handling conditions

--- lib/GenTest/Transform/DisableOptimizations.pm
+++ lib/GenTest/Transform/DisableOptimizations.pm
@@ -16,6 +16,10 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 # USA
 
+#
+# This Transformer simply disables all optimizer switches except for in_to_exists
+#
+
 package GenTest::Transform::DisableOptimizations;
 
 require Exporter;
@@ -27,24 +31,20 @@ use GenTest;
 use GenTest::Transform;
 use GenTest::Constants;
 
-#
-# This Transformer simply disables all optimizer switches except for in_to_exists
-#
-
 sub transform {
 	my ($class, $original_query, $executor, $original_result, $skip_result_validations) = @_;
 
-		return STATUS_WONT_HANDLE 
-	if ( $skip_result_validations 
-			and $original_query !~ m{^\s*(SELECT|UPDATE|DELETE|CREATE\s+OR\s+REPLACE\s+?TABLE.+SELECT|INSERT.+SELECT)}sio )
-		or ( ( ! $skip_result_validations and ( $original_query !~ m{^\s*(SELECT)}sio or $original_query =~ m{\sINTO\s}sio ) ) ) ;
+  return STATUS_WONT_HANDLE if
+    ($original_query !~ /SELECT/)
+      or ($original_query =~ /(?:INTO|CREATE|INSERT.+SELECT)/is)
+      or (! $skip_result_validations and $original_query =~ /INFORMATION_SCHEMA|PERFORMANCE_SCHEMA/is);
 
-	return [ [
-		"SET \@switch_saved = \@\@optimizer_switch",
-		"SET SESSION optimizer_switch = REPLACE(REPLACE( \@\@optimizer_switch, '=on', '=off' ), 'in_to_exists=off', 'in_to_exists=on')",
-		"$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */" ],
-		[ "/* TRANSFORM_CLEANUP */ SET SESSION optimizer_switch=\@switch_saved" ]
-	];
+  return [
+    [ "SET \@switch_saved = \@\@optimizer_switch",
+      "SET SESSION optimizer_switch = REPLACE(REPLACE( \@\@optimizer_switch, '=on', '=off' ), 'in_to_exists=off', 'in_to_exists=on')",
+      "$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */" ],
+    [ "/* TRANSFORM_CLEANUP */ SET SESSION optimizer_switch=\@switch_saved" ]
+  ];
 }
 
 1;
--- lib/GenTest/Transform/EnableOptimizations.pm
+++ lib/GenTest/Transform/EnableOptimizations.pm
@@ -1,5 +1,6 @@
 # Copyright (c) 2008, 2012 Oracle and/or its affiliates. All rights reserved.
 # Copyright (c) 2014 SkySQL Ab
+# Copyright (c) 2021 MariaDB Corporation Ab.
 # Use is subject to license terms.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,6 +17,10 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 # USA
 
+#
+# This Transformer simply enables ALL optimizer switches
+#
+
 package GenTest::Transform::EnableOptimizations;
 
 require Exporter;
@@ -26,25 +31,21 @@ use lib 'lib';
 use GenTest;
 use GenTest::Transform;
 use GenTest::Constants;
-use Carp;
-#
-# This Transformer simply enables ALL optimizer switches 
-#
 
 sub transform {
 	my ($class, $original_query, $executor, $original_result, $skip_result_validations) = @_;
 
-		return STATUS_WONT_HANDLE 
-	if ( $skip_result_validations 
-			and $original_query !~ m{^\s*(SELECT|UPDATE|DELETE|CREATE\s+OR\s+REPLACE\s+?TABLE.+SELECT|INSERT.+SELECT)}sio )
-		or ( ( ! $skip_result_validations and ( $original_query !~ m{^\s*(SELECT)}sio or $original_query =~ m{\sINTO\s}sio ) ) ) ;
+  return STATUS_WONT_HANDLE if
+    ($original_query !~ /SELECT/)
+      or ($original_query =~ /(?:INTO|CREATE|INSERT.+SELECT)/is)
+      or (! $skip_result_validations and $original_query =~ /INFORMATION_SCHEMA|PERFORMANCE_SCHEMA/is);
 
-	return [
-		[ "SET \@switch_saved = \@\@optimizer_switch",
-		"SET SESSION optimizer_switch = REPLACE( \@\@optimizer_switch, '=off', '=on' )",
-		"$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */" ],
-		[ "/* TRANSFORM_CLEANUP */ SET SESSION optimizer_switch=\@switch_saved" ]
-	];
+  return [
+    [ "SET \@switch_saved = \@\@optimizer_switch",
+      "SET SESSION optimizer_switch = REPLACE( \@\@optimizer_switch, '=off', '=on' )",
+      "$original_query /* TRANSFORM_OUTCOME_UNORDERED_MATCH */" ],
+    [ "/* TRANSFORM_CLEANUP */ SET SESSION optimizer_switch=\@switch_saved" ]
+  ];
 }
 
 1;
