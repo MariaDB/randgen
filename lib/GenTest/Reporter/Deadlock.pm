1e7bfadc4533acfa1c5f71c3c85b6628bb0d6a85
Elena Stepanova <elenst@mariadb.com>
2021-03-02 00:23:18 +0200

    Fix typo

--- lib/GenTest/Reporter/Deadlock.pm
+++ lib/GenTest/Reporter/Deadlock.pm
@@ -98,7 +98,7 @@ sub monitor_nonthreaded {
   # Due to MDEV-24998, connect here sometimes returns error 2013
   # falsely indicating server crash. To avoid it, we will try twice before giving up
   unless ($dbh = DBI->connect($dsn, undef, undef, { mysql_connect_timeout => CONNECT_TIMEOUT_THRESHOLD * 2} )) {
-    logWarning("Deadlock report got error ".$DBI::err." upon connecting to $dsn. Trying again");
+    sayWarning("Deadlock report got error ".$DBI::err." upon connecting to $dsn. Trying again");
     $dbh = DBI->connect($dsn, undef, undef, { mysql_connect_timeout => CONNECT_TIMEOUT_THRESHOLD * 2} );
   }
 	if (defined GenTest::Executor::MySQL::errorType($DBI::err)) {
