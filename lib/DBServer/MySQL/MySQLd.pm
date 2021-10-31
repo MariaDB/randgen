120378d52f40fe190ca39d89731e004533f12784
Elena Stepanova <elenst@mariadb.com>
2021-10-31 20:51:02 +0200

    Aria recovery can take long if it doesn't fail right away

--- lib/DBServer/MySQL/MySQLd.pm
+++ lib/DBServer/MySQL/MySQLd.pm
@@ -509,7 +509,12 @@ sub startServer {
             Time::HiRes::sleep($wait_time);
             $errlog_update= ( (stat($errorlog))[9] > $errlog_last_update_time);
             if ($errlog_update) {
-              say("Pid file does not exist and timeout hasn't passed yet, but the error log has already been updated");
+              say("Pid file " . $self->pidfile . " does not exist and timeout hasn't passed yet, but the error log has already been updated");
+              # MySQL 8.0 and MariaDB 10.6+ are slow at creating pid file
+              for (1..60) {
+                last if -f $self->pidfile;
+                sleep 1;
+              }
               last;
             } else {
               say("Pid file hasn't been found yet, waiting");
