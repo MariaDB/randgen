fcb92f6d4ff4479aa7756d6f2e0b63a26367fcbe
Elena Stepanova <elenst@mariadb.com>
2022-01-22 00:05:18 +0200

    10.8 combinations

---
+++ conf/mariadb/regression/all_combinations-10.8.cc
@@ -0,0 +1,85 @@
+use strict;
+
+our ($non_innodb_encryption_options, $innodb_encryption_options, $common_options_108, $ps_protocol_options, $perfschema_options_108);
+our ($grammars_any_gendata, $grammars_specific_gendata, $gendata_files, $auto_gendata_combinations, $optional_redefines_108);
+our ($views_combinations, $vcols_combinations, $threads_low_combinations, $binlog_combinations);
+our ($optional_variators_108, $basic_engine_combinations_108);
+our ($non_crash_scenario_combinations_108, $scenario_combinations_108);
+our ($optional_innodb_variables_108, $optional_plugins_108, $optional_server_variables_108, $optional_charsets_108);
+
+require "$ENV{RQG_HOME}/conf/mariadb/include/parameter_presets";
+require "$ENV{RQG_HOME}/conf/mariadb/include/combo.grammars";
+
+$combinations = [
+  [ @$common_options_108 ], # seed, reporters, timeouts
+  [ '--duration=350' ],
+  [ @$threads_low_combinations ],
+  [ @$views_combinations, '', '', '' ],
+  [ @$vcols_combinations, '--vcols=STORED',
+   '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''
+  ],
+
+  # Combinations of grammars and gendata
+  [
+    {
+      specific =>
+        [ @$grammars_specific_gendata ],
+
+      generic => [
+        [ @$grammars_any_gendata ],
+        [ '--short-column-names' ],
+        [ @$gendata_files ],
+        [ @$auto_gendata_combinations,
+          '--gendata-advanced', '--gendata-advanced', '--gendata-advanced'
+        ],
+      ],
+    }
+  ],
+  ##### Transformers
+  [ {
+      transform => [
+        [ '--validators=TransformerNoComparator' ], @$optional_variators_108,
+      ],
+      notransform => [ '' ]
+    }
+  ],
+  ##### Engines and engine=specific options
+  [
+    {
+      engines => [
+        [ @$basic_engine_combinations_108 ],
+        [ @$non_crash_scenario_combinations_108,
+          '', '', '', '', '', '', '', '', '', '', ''
+        ],
+      ],
+      innodb => [
+        [ '--engine=InnoDB' ],
+        [ @$scenario_combinations_108,
+          '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+        ],
+        @$optional_innodb_variables_108,
+      ],
+    }
+  ],
+  [ @$optional_redefines_108 ],
+  [ @$optional_plugins_108 ],
+  ##### PS protocol and low values of max-prepared-stmt-count
+  [ '', '', '', '', '', '', '', '', '', '',
+    $ps_protocol_options,
+    '--mysqld=--max-prepared-stmt-count=0',
+    '--mysqld=--max-prepared-stmt-count=1',
+  ],
+  ##### Encryption
+  [ '', '', '', '', $non_innodb_encryption_options ],
+  ##### Binary logging
+  [ '', '',
+    [ @$binlog_combinations ],
+  ],
+  ##### Performance schema
+  [ '', '', '',
+    $perfschema_options_108 . ' --redefine=conf/runtime/performance_schema.yy',
+  ],
+  ##### Startup variables (general)
+  [ @$optional_server_variables_108 ],
+  [ @$optional_charsets_108 ],
+];
