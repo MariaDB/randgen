d0d524aa625e55155e13b52f12fd4ae87f3e43c9
Elena Stepanova <elenst@mariadb.com>
2023-07-11 21:56:11 +0300

    Updated upgrade combinations

--- conf/cc/upgrade-normal.cc
+++ conf/cc/upgrade-normal.cc
@@ -18,28 +18,53 @@
 
 use strict;
 
-our ($all_encryption_options, $innodb_pagesize_combinations, $innodb_compression_combinations);
-require "$ENV{RQG_HOME}/conf/mariadb/include/parameter_presets";
+our (%server_options);
+
+# Version should be defined in the combinations options
+$version='999999' unless defined $version;
+
+require "$ENV{RQG_HOME}/conf/cc/include/parameter_presets";
+require "$ENV{RQG_HOME}/conf/cc/include/combo.grammars";
+
+my %options;
+
+foreach my $comb (keys %server_options) {
+  my @opts= ();
+  my $opts= $server_options{$comb};
+  VERSIONS:
+  foreach my $ver (reverse sort keys %$opts) {
+    if ($ver le $version) {
+      push @opts, (ref $opts->{$ver} eq 'ARRAY' ? @{$opts->{$ver}} : $opts->{$ver});
+      last VERSIONS;
+    }
+  }
+  $options{$comb}= [ @opts ];
+}
 
 $combinations = [
   [
   '
-  --threads=2
-  --duration=120
-  --seed=time
-  --redefine=conf/mariadb/modules/acl.yy
-  --gendata=conf/mariadb/innodb.zz
-  --gendata=conf/mariadb/innodb_compression.zz
-  --gendata-advanced
-  --reporters=Backtrace,ErrorLog,Deadlock
-  --mysqld=--server-id=111
-  --mysqld=--log_output=FILE
-  --mysqld=--loose-max-statement-time=20
-  --mysqld=--lock-wait-timeout=10
-  --mysqld=--innodb-lock-wait-timeout=5
-  --scenario=NormalUpgrades
+    --threads=2
+    --duration=120
+    --seed=time
+    --grammar=conf/yy/acl.yy
+    --grammar=conf/yy/dml.yy
+    --grammar=conf/yy/alter_table.yy
+    --gendata=conf/zz/innodb.zz
+    --gendata=conf/zz/innodb-page-compression.zz
+    --gendata=advanced
+    --views
+    --vcols
+    --partitions
+    --reporters=Backtrace,ErrorLog,Deadlock
+    --mysqld=--server-id=111
+    --mysqld=--log_output=FILE
+    --mysqld=--loose-max-statement-time=20
+    --mysqld=--lock-wait-timeout=10
+    --mysqld=--innodb-lock-wait-timeout=5
+    --scenario=NormalUpgrades
   '],
-  [ @$innodb_compression_combinations ],
-  [ @$innodb_pagesize_combinations ],
-  [ '', $all_encryption_options ],
+  [ @{$options{innodb_compression_combinations}} ],
+  [ @{$options{innodb_pagesize_combinations}} ],
+  [ '', $options{all_encryption_options} ],
 ];
--- conf/cc/upgrade.cc
+++ conf/cc/upgrade.cc
@@ -18,28 +18,50 @@
 
 use strict;
 
-our ($all_encryption_options, $upgrade_scenarios, $innodb_pagesize_combinations, $innodb_compression_combinations);
-require "$ENV{RQG_HOME}/conf/mariadb/include/parameter_presets";
-require "$ENV{RQG_HOME}/conf/mariadb/include/upgrade.scenarios";
+our (%server_options);
+
+# Version should be defined in the combinations options
+$version='999999' unless defined $version;
+
+require "$ENV{RQG_HOME}/conf/cc/include/parameter_presets";
+require "$ENV{RQG_HOME}/conf/cc/include/combo.grammars";
+
+my %options;
+
+foreach my $comb (keys %server_options) {
+  my @opts= ();
+  my $opts= $server_options{$comb};
+  VERSIONS:
+  foreach my $ver (reverse sort keys %$opts) {
+    if ($ver le $version) {
+      push @opts, (ref $opts->{$ver} eq 'ARRAY' ? @{$opts->{$ver}} : $opts->{$ver});
+      last VERSIONS;
+    }
+  }
+  $options{$comb}= [ @opts ];
+}
 
 $combinations = [
-  [
-  '
-  --threads=4
-  --seed=time
-  --gendata=conf/mariadb/innodb.zz
-  --gendata=conf/mariadb/innodb_upgrade.zz
-  --gendata-advanced
-  --reporters=Backtrace,ErrorLog,Deadlock
-  --mysqld=--server-id=111
-  --mysqld=--log_output=FILE
-  --mysqld=--loose-max-statement-time=20
-  --mysqld=--lock-wait-timeout=10
-  --mysqld=--innodb-lock-wait-timeout=5
+  ['
+    --threads=2
+    --seed=time
+    --grammar=conf/yy/dml.yy
+    --gendata=conf/zz/innodb.zz
+    --gendata=conf/zz/innodb-page-compression.zz
+    --gendata=advanced
+    --reporters=Backtrace,ErrorLog,Deadlock
+    --mysqld=--server-id=111
+    --mysqld=--log_output=FILE
+    --mysqld=--loose-max-statement-time=20
+    --mysqld=--lock-wait-timeout=10
+    --mysqld=--innodb-lock-wait-timeout=5
   '],
-  [ @$innodb_compression_combinations ],
-  [ @$innodb_pagesize_combinations ],
-  [ '', $all_encryption_options ],
-  # Scenarios
-    $upgrade_scenarios,
+  [ @{$options{innodb_compression_combinations}} ],
+  [ @{$options{innodb_pagesize_combinations}} ],
+  [ '', $options{all_encryption_options} ],
+  [
+    '--scenario=NormalUpgrades --duration=180',
+    '--scenario=CrashUpgrade --duration=180',
+    '--scenario=UndoLogUpgrade --duration=300',
+  ]
 ];
