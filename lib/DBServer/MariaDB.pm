7c5210f971a36d7781ea82a2cdcc4a116af2eace
Elena Stepanova <elenst@mariadb.com>
2024-01-07 21:12:44 +0200

    Remove debug output

--- lib/DBServer/MariaDB.pm
+++ lib/DBServer/MariaDB.pm
@@ -2394,7 +2394,6 @@ sub storeMetaData {
     }
     while (<PROC>) {
       chomp;
-      say("HERE: $_");
       my ($schema, $proc, $type) = split /;/, $_;
       $type= lc($type);
       # paramnum will be just a placeholder for now
