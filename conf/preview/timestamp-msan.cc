51b338bdf3cd6552c8dc011785cdf4634f71e488
Elena Stepanova <elenst@mariadb.com>
2024-04-02 21:37:47 +0300

    Grammar and combination files for MDEV-32188 (32-bit timestamps)

---
+++ conf/preview/timestamp-msan.cc
@@ -0,0 +1,89 @@
+# Copyright (c) 2022, 2023 MariaDB
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+########################################################################
+
+use Data::Dumper;
+use strict;
+
+our (%parameters, %options);
+
+require "$ENV{RQG_HOME}/conf/cc/include/parameter_presets";
+
+# Choose options based on $version value
+# ($version may be defined via config-version, otherwise 999999 will be used)
+local @ARGV = ($version);
+require "$ENV{RQG_HOME}/conf/cc/include/versioned_options.pl";
+
+$combinations = [
+
+# Test options
+  [ @{$options{test_common_option_combinations}} ], # seed, reporters, timeouts
+  [ @{$options{test_concurrency_combinations}} ],
+  [ @{$options{optional_gendata_views}} ],
+  [ @{$options{optional_variators}} ],
+  [ @{$options{gendata}} ],
+
+# Server options
+  [ @{$options{optional_aria_variables}} ],
+  [ @{$options{optional_binlog_safe_variables}} ],
+  [ @{$options{optional_innodb_compression_msan_safe}} ],
+  [ @{$options{optional_innodb_pagesize}} ],
+  [ @{$options{optional_innodb_variables}} ],
+  [ @{$options{optional_perfschema}} ],
+  [ @{$options{optional_server_variables}} ],
+
+# New
+  [ '--grammar=conf/preview/timestamp.yy:0.05' ],
+
+  ##### Engines and scenarios
+  [
+    {
+      normal => [
+        [ @{$options{scenario_non_crash_combinations}} ],
+        [ @{$options{engine_basic_combinations}} ],
+        [ @{$options{optional_charsets_safe}} ],
+        [ @{$options{optional_encryption_msan_safe}} ],
+        [ @{$options{read_only_grammars}}, @{$options{dml_grammars}}, @{$options{ddl_grammars}}, @{$options{variables_grammars}}, @{$options{debug_grammars}} ],
+      ],
+      binlog => [
+        [ '--mysqld=--log-bin' ],
+        [ '--reporter=BinlogDump' ],
+        [ @{$options{scenario_non_crash_combinations}} ],
+        [ @{$options{engine_basic_combinations}} ],
+        [ @{$options{optional_charsets_safe}} ],
+        [ @{$options{optional_encryption_msan_safe}} ],
+        [ @{$options{read_only_grammars}}, @{$options{dml_grammars}}, @{$options{ddl_grammars}}, @{$options{variables_grammars}}, @{$options{debug_grammars}} ],
+      ],
+      index => [
+        [ @{$options{scenario_non_crash_combinations}} ],
+        [ '--reporters=SecondaryIndexConsistency' ],
+        [ @{$options{engine_basic_combinations}} ],
+        [ @{$options{optional_charsets_safe}} ],
+        [ @{$options{optional_encryption_msan_safe}} ],
+        [ @{$options{read_only_grammars}}, @{$options{dml_grammars}}, @{$options{ddl_grammars}}, @{$options{variables_grammars}}, @{$options{debug_grammars}} ],
+      ],
+      recovery => [
+        [ @{$options{scenario_crash_combinations}} ],
+        [ '--engine=InnoDB' ],
+        [ '--filter=conf/ff/restrict_dynamic_vars.ff' ],
+        [ @{$options{optional_charsets_safe}} ],
+        [ @{$options{optional_encryption_msan_safe}} ],
+        [ @{$options{read_only_grammars}}, @{$options{dml_grammars}}, @{$options{ddl_grammars}} ],
+      ],
+    }
+  ],
+];
