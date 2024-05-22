b642ba8bc1b666595f4faa9b7e73f425fe73792c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-22 19:22:21 +0300

    fianl

--- gendata-old.pl
+++ gendata-old.pl
@@ -17,8 +17,9 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 # USA
 
-$| = 1;
+#!/usr/bin/env perl
 use strict;
+use warnings;
 use lib 'lib';
 use lib "$ENV{RQG_HOME}/lib";
 use DBI;
@@ -29,42 +30,48 @@ use GenTest::App::GendataSimple;
 
 my ($dsn, $engine, $help, $views, $notnull);
 
+# Save the original command line arguments
 my @ARGV_saved = @ARGV;
 
+# Process command line options
 my $opt_result = GetOptions(
-	'dsn=s' => \$dsn,
-	'engine:s' => \$engine,
-	'help' => \$help,
-	'views' => \$views,
-	'notnull' => \$notnull
+    'dsn=s'     => \$dsn,
+    'engine:s'  => \$engine,
+    'help'      => \$help,
+    'views'     => \$views,
+    'notnull'   => \$notnull,
 );
 
+# Get the default DSN from the application
 my $default_dsn = GenTest::App::GendataSimple->defaultDsn();
 
+# Show help message if the options parsing failed or if help was requested
 help() if !$opt_result || $help;
 
-my $app = GenTest::App::GendataSimple->new(dsn => $dsn,
-                                           engine => $engine,
-                                           views => $views,
-					   notnull => $notnull);
+# Create the application instance with the provided options
+my $app = GenTest::App::GendataSimple->new(
+    dsn     => $dsn,
+    engine  => $engine,
+    views   => $views,
+    notnull => $notnull,
+);
 
-say("Starting \n# $0 \\ \n# ".join(" \\ \n# ", @ARGV_saved));
+# Print the command line used to start the script
+print "Starting\n# $0 \\\n# " . join(" \\\n# ", @ARGV_saved) . "\n";
 
+# Run the application and exit with the returned status
 my $status = $app->run();
-
 exit $status;
 
+# Subroutine to print help message and exit
 sub help {
-print <<EOF
+    print <<EOF;
 $0 - Simple data generator. Options:
 
     --dsn       : MySQL DBI resource to connect to (default $default_dsn)
-    --engine    : Table engine to use when creating tables (default: no ENGINE in CREATE TABLE )
+    --engine    : Table engine to use when creating tables (default: no ENGINE in CREATE TABLE)
     --views     : Generate views
-    --notnull	: Generate all fields with NOT NULL
-    --help      : This help message 
+    --notnull   : Generate all fields with NOT NULL
+    --help      : This help message
 EOF
-;
-	safe_exit(1);
-}
-
+    exit 1
