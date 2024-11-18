4894e41d90d6c0030f92a14165244d635524cafb
Susil <susil.behera@mariadb.com>
2024-11-18 23:32:32 +0530

    Some formatting

--- xa_recover_check.sh
+++ xa_recover_check.sh
@@ -7,8 +7,8 @@ dbport=10558
 usage(){
 echo "Usage: [ options ]"
 echo "Options:"
-echo "  -m, --master Master info in the format '--host=<hostname> | --port=<port number> | --database=<database> | --user=<db user> | --password=<db password>'"
-echo "  -s, --slave  Slave info in the format '--host=<hostname> | --port=<port number> | --database=<database> | --user=<db user> | --password=<db password>'"
+echo "  -m, --master Master info in the format 'hostname:port:database:user:password'"
+echo "  -s, --slave  Slave info in the format 'hostname:port:database:user:password'"
 }
 IN_ARGS=$(getopt -o m:s:h --long master:,slave:,help -- "$@")
 if [[ $? -ne 0 ]]; then
@@ -45,21 +45,21 @@ fi
 
 for server_info in ${master_info} ${slave_info}
 do
-echo "Apply XA RECOVER on server: $server_info"
-IFS=':' read -r -a connect_info <<< "${server_info}"
-host=${connect_info[0]}
-port=${connect_info[1]}
-database=${connect_info[2]}
-user=${connect_info[3]}
-password=${connect_info[4]}
-db_connect_str="--host=$host --port=$port --database=$database --user=$user --password=$password"
-results=($(mariadb $db_connect_str -Bse "XA RECOVER;"))
-re='^[0-9]+$'
-for row in "${results[@]}"
-do
-  if ! [[ $row =~ $re ]] ; then
-    echo "mariadb $db_connect_str -Bse \"XA COMMIT '$row';\""
-    mariadb $db_connect_str  -Bse "XA COMMIT '$row';"
-  fi
-done
+  echo "Apply XA RECOVER on server: $server_info"
+  IFS=':' read -r -a connect_info <<< "${server_info}"
+  host=${connect_info[0]}
+  port=${connect_info[1]}
+  database=${connect_info[2]}
+  user=${connect_info[3]}
+  password=${connect_info[4]}
+  db_connect_str="--host=$host --port=$port --database=$database --user=$user --password=$password"
+  results=($(mariadb $db_connect_str -Bse "XA RECOVER;"))
+  re='^[0-9]+$'
+  for row in "${results[@]}"
+  do
+    if ! [[ $row =~ $re ]] ; then
+      echo "mariadb $db_connect_str -Bse \"XA COMMIT '$row';\""
+      mariadb $db_connect_str  -Bse "XA COMMIT '$row';"
+    fi
+  done
 done
