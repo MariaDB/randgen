1e994f3f0a2112e6bba686e64e6a9595e0908cea
Elena Stepanova <elenst@mariadb.com>
2025-07-10 22:58:02 +0300

    Imitate directory naming from combinations
    For compatibility with scripts

--- util/combinations_batch.sh
+++ util/combinations_batch.sh
@@ -83,7 +83,7 @@ else
       sleep 1
       cp $workdir/var/trial.log  $archive/trial${t}.log
       if [ -z "$discard_logs" ] && [ "$res" != "0" ] ; then
-        cp -r $workdir/var $archive/vardir${t}
+        cp -r $workdir/var $archive/vardir1_${t}
       fi
     fi
   done < $workdir/combinations.txt
