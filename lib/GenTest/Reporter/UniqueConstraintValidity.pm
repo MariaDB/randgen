d59fb696372591ca86525204608d5aa5125d93f7
Elena Stepanova <elenst@mariadb.com>
2025-07-27 20:27:23 +0300

    Error handling on both queries which the reporter executes

--- lib/GenTest/Reporter/UniqueConstraintValidity.pm
+++ lib/GenTest/Reporter/UniqueConstraintValidity.pm
@@ -41,8 +41,12 @@ sub report {
     return STATUS_SERVER_UNAVAILABLE;
   }
   my $indexes = $conn->query("set statement max_statement_time=0 for select concat('`',table_schema,'`.`',table_name,'`') as tbl, index_name, group_concat(concat('`',column_name,'`')) cols, index_type from INFORMATION_SCHEMA.STATISTICS where non_unique=0 group by tbl, index_name order by tbl, index_name");
-  if ($conn->err or not $indexes) {
-    sayError("UniqueConstraintValidity: could not retrieve unique indexes: ".$conn->print_error());
+  if (ignorable_error($conn->err)) {
+    sayWarning("UniqueConstraintValidity: Got error ".$conn->print_error()." upong retrieving indexes, skipping the check");
+    return STATUS_OK;
+  }
+  elsif ($conn->err or not $indexes) {
+    sayError("UniqueConstraintValidity: could not retrieve unique indexes: ".$conn->print_error().", , returning STATUS_DATABASE_CORRUPTION");
     return STATUS_DATABASE_CORRUPTION;
   }
   my $res=STATUS_OK;
@@ -50,25 +54,13 @@ sub report {
     my ($tbl, $ind, $cols, $tp)= @$tbl_ind;
     my $non_null = join ' AND ', (map { "$_ IS NOT NULL" } split /,/, $cols);
     my $multiple_results= $conn->query("set statement max_statement_time=0 for select $cols, count(*) cnt from $tbl WHERE $non_null group by $cols having cnt > 1");
-    # Ignore certain errors related to engine specifics, we are here not for this
-    # 1159: Got timeout reading communication packets (Spider)
-    # 1168: Unable to open underlying table (Merge)
-    # 1296: Got error 122 'Open error 2 in mode rb on... (Connect)
-    # 1429: Unable to connect to foreign data source (Spider)
-    # 12719: An infinite loop is detected when opening table (Spider)
-    if (
-      ($conn->err == 1159) ||
-      ($conn->err == 1168) ||
-      ($conn->err == 1296) ||
-      ($conn->err == 1429) ||
-      ($conn->err == 12719)
-    ) {
+    if (ignorable_error($conn->err)) {
       sayWarning("UniqueConstraintValidity: Got error ".$conn->print_error()." for $tbl, ignoring");
     } elsif ($conn->err or not $multiple_results) {
       sayError("UniqueConstraintValidity: could not perform counts on unique indexes: ".$conn->print_error());
       return STATUS_DATABASE_CORRUPTION;
     } elsif (scalar(@$multiple_results)) {
-      sayError("UniqueConstraintValidity: unique constraint $ind ($cols) of type $tp on table $tbl contains non-unique values");
+      sayError("UniqueConstraintValidity: unique constraint $ind ($cols) of type $tp on table $tbl contains non-unique values, returning STATUS_DATABASE_CORRUPTION");
       foreach my $vals (@$multiple_results) {
         my $cnt= pop @$vals;
         say('Values: "'.(join ',', (map { defined $_ ? $_ : '<null>' } @$vals) ).'" : count '.$cnt);
@@ -77,6 +69,27 @@ sub report {
     }
   }
   return $res;
+}
+
+sub ignorable_error {
+  my $err= shift;
+  # Ignore certain errors related to engine specifics and alike, we are here not for this
+  # 1047: WSREP has not yet prepared node for application use
+  # 1159: Got timeout reading communication packets (Spider)
+  # 1168: Unable to open underlying table (Merge)
+  # 1296: Got error 122 'Open error 2 in mode rb on... (Connect)
+  # 1429: Unable to connect to foreign data source (Spider)
+  # 12702: Remote table ... is not found
+  # 12719: An infinite loop is detected when opening table (Spider)
+  return (
+    ($err == 1047) ||
+    ($err == 1159) ||
+    ($err == 1168) ||
+    ($err == 1296) ||
+    ($err == 1429) ||
+    ($err == 12702) ||
+    ($err == 12719)
+  );
 }
 
 sub monitor {
