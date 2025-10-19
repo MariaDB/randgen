1a4f2b884778b2b5a09f08818c8a11dece525de9
Elena Stepanova <elenst@mariadb.com>
2025-10-19 17:27:32 +0300

    Better backtrace reporting

--- lib/GenTest/Scenario.pm
+++ lib/GenTest/Scenario.pm
@@ -413,6 +413,7 @@ sub prepareServer {
                       config => $self->[SC_TEST_PROPERTIES]->server_specific->{$srvnum}->{cnf},
                       dataset => $self->[SC_TEST_PROPERTIES]->server_specific->{$srvnum}->{dataset},
                       general_log => 1,
+                      id => $srvnum,
                       manual_gdb => $self->[SC_TEST_PROPERTIES]->server_specific->{$srvnum}->{manual_gdb},
                       port => ($self->[SC_TEST_PROPERTIES]->server_specific->{$srvnum}->{port} || $self->[SC_TEST_PROPERTIES]->base_port + $srvnum - 1),
                       rr => $self->[SC_TEST_PROPERTIES]->server_specific->{$srvnum}->{rr},
