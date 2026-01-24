53486d725e657f0e94c207f37610164d010b6f26
Elena Stepanova <elenst@mariadb.com>
2026-01-24 14:35:58 +0200

    Make signatures configurable
    So that for example preview signatures would only be used when
    needed

--- util/combinations_batch.sh
+++ util/combinations_batch.sh
@@ -32,6 +32,7 @@ port_group=14000
 workdir=""
 archive=""
 discard_logs=""
+signatures="util/bug_signatures"
 
 for arg in "$@" ; do
   val=`echo "$arg" | sed -e "s;--[^=]*=;;"`
@@ -40,6 +41,7 @@ for arg in "$@" ; do
     --workdir=*|--vardir=*) workdir="$val" ;;
     --archive=*)            archive="$val" ;;
     --discard-logs*)        discard_logs=1 ;;
+    --signatures=*)        signatures=$val ;;
     *)                      opts="$opts $arg" ;;
   esac
 done
@@ -85,7 +87,7 @@ else
       echo "###################################" | tee -a $archive/results.txt
       echo "Log: trial${t}.log" | tee -a $archive/results.txt
       if [ "$res" != "0" ] ; then
-        perl util/check_for_known_bugs.pl --signatures=util/bug_signatures* $workdir/var/s*/mysql.err $archive/trial${t}.log $workdir/var/s*/boot.log 2>&1 | tee -a $archive/results.txt
+        perl util/check_for_known_bugs.pl --signatures=$signatures $workdir/var/s*/mysql.err $archive/trial${t}.log $workdir/var/s*/boot.log 2>&1 | tee -a $archive/results.txt
       else
         grep -a 'Test run ends with exit status' $archive/trial${t}.log >> $archive/results.txt
       fi
