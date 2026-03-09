8287828a47c1aca70642c36dc083dd09f09675c2
Elena Stepanova <elenst@mariadb.com>
2026-03-09 20:06:25 +0200

    Explicitly set --nopartitions for Aria encrypted run
    To avoid creating partitioned tables from .zz files

--- conf/cc/upgrade-static.cc
+++ conf/cc/upgrade-static.cc
@@ -78,15 +78,16 @@ $combinations = [
   myisam =>
     [ ['--engine=MyISAM,Aria'],
       [ '--partitions',
-        '--mysqld=--innodb-encrypt-tables
-        --mysqld=--innodb-encrypt-log
-        --mysqld=--innodb-encryption-threads=4
-        --mysqld=--aria-encrypt-tables=1
-        --mysqld=--encrypt-tmp-disk-tables=1
-        --mysqld=--encrypt-binlog
-        --mysqld=--file-key-management
-        --mysqld=--file-key-management-filename='.$ENV{RQG_HOME}.'/util/file_key_management_keys.txt
-        --mysqld=--plugin-load-add=file_key_management
+        '--nopartitions
+         --mysqld=--innodb-encrypt-tables
+         --mysqld=--innodb-encrypt-log
+         --mysqld=--innodb-encryption-threads=4
+         --mysqld=--aria-encrypt-tables=1
+         --mysqld=--encrypt-tmp-disk-tables=1
+         --mysqld=--encrypt-binlog
+         --mysqld=--file-key-management
+         --mysqld=--file-key-management-filename='.$ENV{RQG_HOME}.'/util/file_key_management_keys.txt
+         --mysqld=--plugin-load-add=file_key_management
         '
       ],
     ],
